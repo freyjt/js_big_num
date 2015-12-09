b72c75b2fa5ad02cc3d815885bcd8d951cece76c
Joshua Frey <freyjt@gmail.com>
2015-12-08 22:36:50 -0500

    Code runs, solution wrong.

--- js_big_num.js
+++ js_big_num.js
@@ -12,20 +12,26 @@ function BigNum(   ) {
 
 //convert a string represented number to a binary string
 BigNum.prototype.convertString = function( stringIn ) {
+    
+    //return binary string representation of number LSB -> MSB
     function binUnderTen( numberIn ) {
         var num = parseInt(numberIn);
         var i;
         var stringOut = "";
-        for(i = 8; i >= 0; i /= 2) {
-            if(num / i > 0) {
-                stringOut.unshift( '1' );
-            } else { stringOut.unshift( '0'); }
+        for(i = 8; i >= 1; i /= 2) {  
+            if(num / i > 0) { //this is very clearly not right
+                stringOut = '1' + stringOut;
+            } else { stringOut = '0' + stringOut; }
         }
+        console.log("binUnderTen:  " + stringOut)
         return stringOut;
     }
+
+
     //helper function, converts a string to a binary string
     // numberIn should never be > 45
     function convPlace( numberIn, place ) {
+        console.log("Call here");
         var locNum    = numberIn.toString();
         var len       = locNum.length;
         var numHolder = "";
@@ -43,8 +49,9 @@ BigNum.prototype.convertString = function( stringIn ) {
 
         smallString = binUnderTen( parseInt(locNum[0]) );
         if( len > 1) {
-            largeString = convPlace( parseInt( locNum.subStr(1) ) * 5, 2 );
-            largeString.unshift( 0 );
+            largeString = convPlace( parseInt( locNum.substring(1) ) * 5, 2 );
+            console.log(largeString);
+            largeString = '0' + largeString;
             //addStrings
             var maxLen  = (smallString.length > largeString.length) ? smallString.length : largeString.length;
             var carrier = 0;
@@ -76,6 +83,7 @@ BigNum.prototype.convertString = function( stringIn ) {
         }
         return stringOut;
     }
+
     this.binString = convPlace( stringIn, 0);
 }
 BigNum.prototype.toStringBin  = function( ) {
--- testDriver.js
+++ testDriver.js
@@ -1,6 +1,8 @@
 
 
 function driver( ) {
+    var string = "String";
+    console.log(string.toString());
     var big = require('./js_big_num.js');
     //
     var num = new big.BigNum( );
