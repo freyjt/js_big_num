c9336e04cb4b78c973e867bdafbf1991362e35ba
Joshua Frey <freyjt@gmail.com>
2015-12-12 17:31:34 -0500

    Even more tests. Still running green.

--- testDriver.js
+++ testDriver.js
@@ -5,7 +5,7 @@ function driver( ) {
     var big = require('./js_big_num.js');
     var i;    
     
-    //*******
+    //*******MULTIPLICATION
     var num  = new big.BigNum( 10 );
     var num2 = new big.BigNum(  0 );
     assertEqual("0", num.multiply(num2).toString(), "asserting 0 = 0 in multiplication" );
@@ -29,7 +29,11 @@ function driver( ) {
     num.convertString( -10 );
     num2.convertString( 0 );
     assertEqual("0", num.multiply(num2).toString(), "asserting -10 * 0 = 0");
-    //*******
+    
+
+
+
+    //*******DIVISION
     num.convertString( "-25" );
     num2.convertString( "5" );
     assertEqual("-5", num.divide( num2 ).toString(), "asserting -5 = -5 with division");
@@ -39,7 +43,22 @@ function driver( ) {
     num2.convertString( "-5" );
     assertEqual("-107", num.divide(num2).toString(), "asserting 537/-5 = -107 with division");
 
-    //*******
+    //********
+    num.convertString( "0" );
+    num2.convertString( 20 );
+    assertEqual("0", num.divide(num2).toString(), "asserting 0/20 == 0 with division");
+
+    //********
+    num.convertString( "-0" );
+    num2.convertString( 20 );
+    assertEqual("0", num.divide(num2).toString(), "asserting -0/20 == 0 with division");
+
+    //********
+    num.convertString("4");
+    num2.convertString(0);
+    assertEqual(null, num.divide(num2), "asserting 4/0 == null with division");
+
+    //*******ADDITION
     num.convertString( "55" );
     assertEqual( "110", num.add(55).toString(), "asserting 110 == 110 with addition");
 
@@ -47,6 +66,14 @@ function driver( ) {
     num.convertString("5017");
     num2.convertString("-5037");
     assertEqual( "-20", num.add(num2).toString(), "asserting -20 == -20 with addition");
+
+    num.convertString("-50");
+    num2.convertString("50");
+    assertEqual( "0", num.add(num2).toString(), "asserting -50 + 50 == 0 with addition");
+
+    num.convertString("50");
+    num2.convertString(-50);
+    assertEqual( "0", num.add(num2).toString(), "asserting 50 + -50 == 0 with addition");
 }
 
 driver();
